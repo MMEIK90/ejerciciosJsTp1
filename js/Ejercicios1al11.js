acbb26fd2d8f3a60c7e2eaccf534a2fb29c2e0fe
Mariano Meik <meik_90@hotmail.com>
2021-09-29 15:33:16 -0300

    detalles esteticos en el ejercicio 9

--- js/Ejercicios1al11.js
+++ js/Ejercicios1al11.js
@@ -67,7 +67,7 @@ let frase = prompt("Escribe una frase");
 let nfrase = frase.length;
 for (let i = 0 ; i < frase.length ; i++){
     if(frase.substr(i,1) === "a" || frase.substr(i,1) === "e" || frase.substr(i,1) === "i" || frase.substr(i,1) === "o" || frase.substr(i,1) === "u")
-document.write( frase.substr(i, (1)) );
+document.write(" " + frase.substr(i, (1)) );
  }
 
 
