5a74240bf14164245a6b84f68ffc3a6b8cc0375f
Mariano Meik <meik_90@hotmail.com>
2021-10-06 15:13:56 -0300

    realice con switch los ejercicios 2 y 5

--- js/ejBucle2.js
+++ js/ejBucle2.js
@@ -1,18 +1,25 @@
 //2
 
 let nota = prompt("Ingrese la nota (del 0 al 10): ");
-if ( 0 <= nota && nota <= 2 ){
-    alert ( "Muy deficiente, su nota es: " + nota)
-} else if ( 2 < nota && nota <= 4 ){
-    alert ("Insuficiente, su nota es: " + nota ) 
- } else if ( 4 < nota && nota <= 6 ){
-    alert ("Suficiente, su nota es: " + nota )
- } else if ( 6 < nota && nota <= 7 ){
-    alert ("Bien, su nota es: " + nota )
- } else if ( 7 < nota && nota <= 9 ){
-    alert ("Notable , su nota es: " + nota )
- } else if ( 9 < nota && nota <= 10 ){
-    alert ("Sobresaliente, su nota es: " + nota )
- } else {
-     alert ("Ingrese un numero valido")      
- }
+ switch (nota){
+    case  "0","1","2":
+       alert ( "Muy deficiente, su nota es: " + nota);
+       break;
+   case "3","4":
+       alert ("Insuficiente, su nota es: " + nota );
+       break;
+   case "5","6":
+       alert ("Suficiente, su nota es: " + nota );
+       break;
+   case "7":
+       alert ("Bien, su nota es: " + nota );
+       break;
+   case "8","9":
+       alert ("Notable , su nota es: " + nota );
+       break;
+   case "10":
+       alert ("Sobresaliente, su nota es: " + nota );
+       break;
+   default: 
+       alert ("Ingrese un numero valido");
+ } 
--- js/ejBucle5.js
+++ js/ejBucle5.js
@@ -3,62 +3,86 @@ do{
     let numero = prompt("Introduce un numero de DNI (sin puntos"); 
         if (Number(numero) == numero) { 
               numero = Number (numero);
-          } 
+          
     if(numero > 0 && numero < 99999999) {
     
         resultado = numero % 23;
         
-        if (resultado === 0){
-            document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: T ")
-        } else if (resultado === 1){
-            document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: R ")
-    } else if (resultado === 2){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: W ")
-    } else if (resultado === 3){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: A ")
-    } else if (resultado === 4){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: G ")
-    } else if (resultado === 5){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: M ")
-    } else if (resultado === 6){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: Y ")
-    } else if (resultado === 7){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: F ")
-    } else if (resultado === 8){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: P ")
-    } else if (resultado === 9){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: D ")
-    } else if (resultado === 10){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: X ")
-    } else if (resultado === 11){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: B ")
-    } else if (resultado === 12){
-        document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: N ")
-    } else if (resultado === 13){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: J ")
-    } else if (resultado === 14){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: Z ")
-    } else if (resultado === 15){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: S ")
-    } else if (resultado === 16){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: Q ")
-    } else if (resultado === 17){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: V ")
-    } else if (resultado === 18){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: H ")
-    } else if (resultado === 19){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: L ")
-    } else if (resultado === 20){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: C ")
-    } else if (resultado === 21){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: K ")
-    } else if (resultado === 22){
-    document.write("<br>"+ numero + " el valor en letra del numero de DNI ingresado es: E ")
+    switch (resultado){
+        case 0: 
+           document.write( " el valor en letra del numero de DNI ingresado es: T ");
+           break;
+        case 1: 
+           document.write( " el valor en letra del numero de DNI ingresado es: R ");
+           break;
+        case 2:        
+           document.write( " el valor en letra del numero de DNI ingresado es: W ");
+           break;
+        case 3:        
+           document.write(" el valor en letra del numero de DNI ingresado es: A ");
+           break;
+        case 4:
+           document.write(" el valor en letra del numero de DNI ingresado es: G ");
+           break;
+        case 5:        
+           document.write(" el valor en letra del numero de DNI ingresado es: M ");
+           break;
+        case 6:        
+           document.write(" el valor en letra del numero de DNI ingresado es: Y ");
+           break;
+        case 7:        
+           document.write( " el valor en letra del numero de DNI ingresado es: F ");
+           break;
+        case 8:       
+           document.write( " el valor en letra del numero de DNI ingresado es: P ");
+           break;
+        case 9:        
+           document.write( " el valor en letra del numero de DNI ingresado es: D ");
+           break;
+        case 10:
+           document.write( " el valor en letra del numero de DNI ingresado es: X ");
+           break;
+        case 11:
+           document.write( " el valor en letra del numero de DNI ingresado es: B ");
+           break;
+        case 12:
+           document.write( " el valor en letra del numero de DNI ingresado es: N ");
+           break;
+        case 13:
+           document.write( " el valor en letra del numero de DNI ingresado es: J ");
+           break;
+        case 14:
+           document.write( " el valor en letra del numero de DNI ingresado es: Z ");
+           break;
+        case 15:
+           document.write( " el valor en letra del numero de DNI ingresado es: S ");
+           break;
+        case 16:
+           document.write( " el valor en letra del numero de DNI ingresado es: Q ");
+           break;
+        case 17:
+           document.write( " el valor en letra del numero de DNI ingresado es: V ");
+           break;
+        case 18:
+           document.write( " el valor en letra del numero de DNI ingresado es: H ");
+           break;
+        case 19:
+           document.write( " el valor en letra del numero de DNI ingresado es: L ");
+           break;
+        case 20:
+           document.write( " el valor en letra del numero de DNI ingresado es: C ");
+           break;
+        case 21:
+           document.write( " el valor en letra del numero de DNI ingresado es: K ");
+           break;
+        case 22:
+           document.write( " el valor en letra del numero de DNI ingresado es: E ");
+           break;
+        default :
+            if (numero != Number(numero)    ) {
+            alert(numero + " No es un numero");
+        }
     } 
     }
-     else  {
-        if (numero != Number(numero)    ) {
-        alert(numero + " No es un numero");
-    }
-    }
-    } while (confirm("desea continuar?"));
+    } 
+    }while (confirm("desea continuar?"));
